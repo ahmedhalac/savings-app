e49eea2186e0d087f40a9c731d5f34b4fc9e9be9
ahmedhalac <ahmedhalac@outlook.com>
2026-05-18 00:35:41 +0200

    Fixed multiple same APIs calling

--- frontend/src/app/features/accounts/services/accounts.service.ts
+++ frontend/src/app/features/accounts/services/accounts.service.ts
@@ -1,5 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { shareReplay, tap } from 'rxjs/operators';
 import { environment } from '../../../../environments/environment';
 import { Account, AccountsSummary } from '../../../models/account';
 
@@ -7,17 +9,21 @@ import { Account, AccountsSummary } from '../../../models/account';
 export class AccountsService {
   private http = inject(HttpClient);
   private base = `${environment.apiBaseUrl}/accounts`;
+  private allCache$: Observable<Account[]> | null = null;
 
   create(data: { name: string; type: 'savings' | 'current' | 'buffer' }) {
-    return this.http.post<Account>(this.base, data);
+    return this.http.post<Account>(this.base, data).pipe(tap(() => this.allCache$ = null));
   }
 
   delete(id: number) {
-    return this.http.delete(`${this.base}/${id}`);
+    return this.http.delete(`${this.base}/${id}`).pipe(tap(() => this.allCache$ = null));
   }
 
   getAll() {
-    return this.http.get<Account[]>(this.base);
+    if (!this.allCache$) {
+      this.allCache$ = this.http.get<Account[]>(this.base).pipe(shareReplay(1));
+    }
+    return this.allCache$;
   }
 
   getSummary() {
