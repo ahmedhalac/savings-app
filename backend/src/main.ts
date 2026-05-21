466fcad0b9dd3dadd0617039915d4c24f38b9ed6
ahmedhalac <ahmedhalac@outlook.com>
2026-05-21 20:44:20 +0200

    Added new frontend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,12 +7,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'https://savings-app-ivory.vercel.app',
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }),
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
   );
   await app.listen(process.env.PORT ?? 3000);
 }
