633c845eaa80f8ef75ca6b316e5d6260c9b8e5a3
ahmedhalac <ahmedhalac@outlook.com>
2026-05-23 22:54:09 +0200

    Fix trustedOrigins

--- backend/src/auth/auth.ts
+++ backend/src/auth/auth.ts
@@ -11,6 +11,7 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  trustedOrigins: [process.env.FRONTEND_URL as string],
   emailAndPassword: {
     enabled: true,
   },
